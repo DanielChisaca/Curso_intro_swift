0e9d4bf08ac479006d931fa0e267949d9f6442b9
Daniel Chisaca <danielchisaca@gmail.com>
2022-12-04 22:45:30 -0500

    01_hello_world Mi primer programa

---
+++ 01_hello_world/01_hello_world/01_hello_world.swift
@@ -0,0 +1,12 @@
+//
+//  01_hello_world.swift
+//  01_hello_world
+//
+//  Created by Daniel Chisacá Rubio on 4/12/22.
+//
+
+import UIKit
+
+var srt = "Hello, playground"
+
+    print("Hola Juan Gabriel!")
--- 01_hello_world/01_hello_world/_1_hello_worldApp.swift
+++ 01_hello_world/01_hello_world/_1_hello_worldApp.swift
@@ -15,3 +15,4 @@ struct _1_hello_worldApp: App {
         }
     }
 }
+
