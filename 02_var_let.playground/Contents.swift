26e035c416bd3ddd74eaeced049a0e307ffaf3a4
Daniel Chisaca <danielchisaca@gmail.com>
2022-12-05 19:18:29 -0500

    02_var_let 02 jugando con emojis

--- 02_var_let.playground/Contents.swift
+++ 02_var_let.playground/Contents.swift
@@ -11,3 +11,13 @@ var welcomeMessage : String
 welcomeMessage = "Hola, ¿que tal?"
 
 var red, green, blue : Double
+
+
+
+let π = 3.141592
+let おはようございます！: String
+var 👽 = "Alien"
+
+print(👽)
+
+👽 = "Alien 2"
